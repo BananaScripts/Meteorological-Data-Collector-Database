70be3b80e3c288b5bef6df124887be985917d62f
Douglas F. Medeiros <80690245+DouglasMedeiros1@users.noreply.github.com>
2024-10-02 18:32:12 -0300

    Add/Deploy-do-Database-no-Supabase

--- src/Database-Prisma/migrations/20241002000649_primeiro_banco/migration.sql
+++ src/Database-Prisma/migrations/20241002000649_primeiro_banco/migration.sql
@@ -1,6 +1,6 @@
 -- CreateTable
 CREATE TABLE "Usuario" (
-    "cod_usuario" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "cod_usuario" SERIAL PRIMARY KEY,
     "nome" TEXT NOT NULL,
     "dataNascimento" TEXT NOT NULL,
     "cpf" TEXT NOT NULL,
@@ -10,7 +10,7 @@ CREATE TABLE "Usuario" (
 
 -- CreateTable
 CREATE TABLE "Estacao" (
-    "cod_estacao" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "cod_estacao" SERIAL PRIMARY KEY,
     "macAdress" TEXT NOT NULL,
     "nome" TEXT NOT NULL,
     "cidade" TEXT NOT NULL,
@@ -21,16 +21,16 @@ CREATE TABLE "Estacao" (
 
 -- CreateTable
 CREATE TABLE "Parametro" (
-    "cod_parametro" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "cod_parametro" SERIAL PRIMARY KEY,
     "cod_estacao" INTEGER NOT NULL,
     "cod_tipoParametro" INTEGER NOT NULL,
-    CONSTRAINT "Parametro_cod_estacao_fkey" FOREIGN KEY ("cod_estacao") REFERENCES "Estacao" ("cod_estacao") ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT "Parametro_cod_tipoParametro_fkey" FOREIGN KEY ("cod_tipoParametro") REFERENCES "TipoParametro" ("cod_tipoParametro") ON DELETE RESTRICT ON UPDATE CASCADE
+    FOREIGN KEY ("cod_estacao") REFERENCES "Estacao" ("cod_estacao") ON DELETE RESTRICT ON UPDATE CASCADE,
+    FOREIGN KEY ("cod_tipoParametro") REFERENCES "TipoParametro" ("cod_tipoParametro") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 -- CreateTable
 CREATE TABLE "TipoParametro" (
-    "cod_tipoParametro" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "cod_tipoParametro" SERIAL PRIMARY KEY,
     "nome" TEXT NOT NULL,
     "fator" TEXT NOT NULL,
     "offset" TEXT NOT NULL,
@@ -39,28 +39,28 @@ CREATE TABLE "TipoParametro" (
 
 -- CreateTable
 CREATE TABLE "Dados" (
-    "cod_dados" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "cod_dados" SERIAL PRIMARY KEY,
     "cod_parametro" INTEGER NOT NULL,
-    CONSTRAINT "Dados_cod_parametro_fkey" FOREIGN KEY ("cod_parametro") REFERENCES "Parametro" ("cod_parametro") ON DELETE RESTRICT ON UPDATE CASCADE
+    FOREIGN KEY ("cod_parametro") REFERENCES "Parametro" ("cod_parametro") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 -- CreateTable
 CREATE TABLE "Alarmes" (
-    "cod_alarme" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "cod_alarme" SERIAL PRIMARY KEY,
     "nome" TEXT NOT NULL,
     "valor" TEXT NOT NULL,
     "condicao" TEXT NOT NULL,
     "cod_parametro" INTEGER NOT NULL,
-    CONSTRAINT "Alarmes_cod_parametro_fkey" FOREIGN KEY ("cod_parametro") REFERENCES "Parametro" ("cod_parametro") ON DELETE RESTRICT ON UPDATE CASCADE
+    FOREIGN KEY ("cod_parametro") REFERENCES "Parametro" ("cod_parametro") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 -- CreateTable
 CREATE TABLE "HistoricoAlarme" (
-    "cod_historicoAlarme" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "cod_historicoAlarme" SERIAL PRIMARY KEY,
     "valor" TEXT NOT NULL,
     "data" TEXT NOT NULL,
     "hora" TEXT NOT NULL,
     "cod_alarmes" INTEGER NOT NULL,
     "cod_alarme" INTEGER NOT NULL,
-    CONSTRAINT "HistoricoAlarme_cod_alarme_fkey" FOREIGN KEY ("cod_alarme") REFERENCES "Alarmes" ("cod_alarme") ON DELETE RESTRICT ON UPDATE CASCADE
+    FOREIGN KEY ("cod_alarme") REFERENCES "Alarmes" ("cod_alarme") ON DELETE RESTRICT ON UPDATE CASCADE
 );
